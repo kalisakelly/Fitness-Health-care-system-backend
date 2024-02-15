2f8d274dec5fdde2a98c925843ff7bcafbd89269
kalisakelly <kalisakelly2001@gmail.com>
2024-02-15 15:23:15 +0200

    updating

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,30 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { DataSource } from 'typeorm';
-import * as joi from '@hapi/joi';
-import { DatabaseModule } from './database/database.module';
-import { ConfigModule } from '@nestjs/config';
-import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
+
 
 @Module({
   imports: [
-    ConfigModule.forRoot(
-      {
-        validationSchema:joi.object({
-          POSTGRES_HOST:joi.string().required(),
-          POSTGRES_PORT:joi.number().required(),
-          POSTGRES_USER:joi.string().required(),
-          POSTGRES_PASSWORD:joi.string().required(),
-          POSTGRES_DB:joi.string().required(),  
-
-        })
-        
-      }),
-    DatabaseModule,
-    UsersModule,
-    AuthModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      password: '1234',
+      username: 'postgres',
+      entities: [], // here we have added user enitity in entities array
+      database: 'Student fitness& health care DB',
+      synchronize: true,
+      logging: true,
+    }),
+    
     
   ],
   controllers: [AppController],
